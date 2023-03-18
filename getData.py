e29d1aca6a9ceb2949c92e0716e7ce5690d5e24b
Irmak Ege <irmakege5@gmail.com>
2023-03-18 21:12:44 +0300

    second commit

--- getData.py
+++ getData.py
@@ -1,11 +1,29 @@
 import pandas as pd
 
 def getMetadata(data):
-    metadata = pd.read_csv("data/prs_metadata.csv")
+    metadata = pd.read_csv("data/prs_metadata_trial.csv")
     variants = metadata.variant.values.tolist()
-    return print(metadata, variants)
+    return metadata, variants
+
+def getPopdata(data):
+    return
+
+def checkGenotype(fileName, metadata):
+    variant = pd.read_csv(f"data/variants1000genome/{fileName}_EUR.csv")
+    row = metadata.loc[metadata['variant'] == fileName]
+    
+    rA1 = row['riskAllele1'].values[0] + row['riskAllele1'].values[0]
+    rA2 = row['riskAllele2'].values[0] + row['riskAllele2'].values[0]
+    rA3 = row['riskAllele1'].values[0] + row['riskAllele2'].values[0]
+    rA4 = row['riskAllele2'].values[0] + row['riskAllele1'].values[0]
+
+    print(rA1, rA2, rA3, rA4)
+
+    return 
 
 
 if __name__ == "__main__":
     metaDataName = "prs_metadata.csv"
-    getMetadata(metaDataName)
+    metadata, variantList = getMetadata(metaDataName)
+    for v in variantList:
+        checkGenotype(v, metadata)
