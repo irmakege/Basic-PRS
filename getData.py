79c2369879a017528737c4f7d97bf05ed7a6c153
Irmak Ege <irmakege5@gmail.com>
2023-03-18 22:45:49 +0300

    third commit

--- getData.py
+++ getData.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import os
 
 def getMetadata(data):
     metadata = pd.read_csv("data/prs_metadata_trial.csv")
@@ -9,21 +10,54 @@ def getPopdata(data):
     return
 
 def checkGenotype(fileName, metadata):
-    variant = pd.read_csv(f"data/variants1000genome/{fileName}_EUR.csv")
+    variant = pd.read_csv(f"data/variants1000genome/{fileName}_EUR.csv")['Genotype (forward strand)'].to_frame()
     row = metadata.loc[metadata['variant'] == fileName]
+
+    rA1  = row['riskAllele1'].values[0]
+    rA2 = row['riskAllele2'].values[0]    
+    rG1 = row['riskAllele1'].values[0] + row['riskAllele1'].values[0]
+    rG2 = row['riskAllele2'].values[0] + row['riskAllele2'].values[0]
+    rG3 = row['riskAllele1'].values[0] + row['riskAllele2'].values[0]
+    rG4 = row['riskAllele2'].values[0] + row['riskAllele1'].values[0]
     
-    rA1 = row['riskAllele1'].values[0] + row['riskAllele1'].values[0]
-    rA2 = row['riskAllele2'].values[0] + row['riskAllele2'].values[0]
-    rA3 = row['riskAllele1'].values[0] + row['riskAllele2'].values[0]
-    rA4 = row['riskAllele2'].values[0] + row['riskAllele1'].values[0]
+    variantScore = []
+    for index, sample in variant.iterrows():
+        if rG1 in sample.values[0] or rG2 in sample.values[0]:
+            score = row['effectSize'].values[0] * 2
+            variantScore.append(score)
+        elif rG3 in sample.values[0] or rG4 in sample.values[0]:
+            score = row['effectSize'].values[0]
+            variantScore.append(score)
+        else: 
+            score = 0
+            variantScore.append(score)
 
-    print(rA1, rA2, rA3, rA4)
+        
+        """ if rA1 not in sample.values[0] or rA2 not in sample.values[0]: 
+            print(f'No match in {fileName}') """
+        variantScoreCol = pd.DataFrame()
+    variantScoreCol[fileName + " Score"] = variantScore
+    
+    
+    
 
-    return 
+    return variantScoreCol
+
+
+    
 
 
 if __name__ == "__main__":
     metaDataName = "prs_metadata.csv"
     metadata, variantList = getMetadata(metaDataName)
+
+    if os.path.isfile('data/prs_trial.xlsx'):
+        os.remove('data/prs_trial.xlsx')
+    polyScore = pd.DataFrame()
+    
     for v in variantList:
-        checkGenotype(v, metadata)
+        col1 = checkGenotype(v, metadata)
+        polyScore[v] = col1
+    
+    polyScore['total'] = polyScore.sum(axis=1)
+    polyScore.to_excel("data/prs_trial.xlsx", index=False)
